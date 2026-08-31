4b28e71d77b367303fda6e08e13a43cadfb05c3b
Parth Mane <parthmane333@gmail.com>
2026-09-01 01:51:46 +0530

    Created functions for every operation

--- main.py
+++ main.py
@@ -2,6 +2,53 @@ tasks = []
 
 choice = 0
 
+def show_tasks():
+    for i in tasks:
+        print(i["title"])
+
+def add_task():
+    print("'Add Task' selected")
+    title = input("Enter task title:")
+    task ={
+        "title": title,
+        "completed": False
+    }
+    tasks.append(task)
+
+def view_task():
+    print("'View Tasks' selected")
+    if tasks == []:
+        print("No tasks found.")
+    for i in tasks:
+        if i["completed"] == True:
+            print(i["title"] + "-- completed")
+        else:
+            print(i["title"] + "-- incomplete")
+
+def mark_task_as_completed():
+    print("'Mark Task as Completed' selected")
+    found = False
+    show_tasks()
+    completed_task_title = input("Select completed task:")
+    for i in tasks:
+        if i["title"] == completed_task_title:
+            found = True
+            i["completed"] = True
+    if not found:
+        print("Task not found.")
+
+def delete_task():
+    print("'Delete Task' selected")
+    found = False
+    show_tasks()  
+    task_to_delete = input("Select task to delete:")
+    for i in tasks:
+        if i["title"] == task_to_delete:
+            found = True
+            tasks.remove(i)
+    if not found:
+        print("Task not found.")
+
 while choice != 5:
     print("===== TO-DO LIST =====")
     print("1. Add Task")
@@ -12,64 +59,19 @@ while choice != 5:
     choice = int(input("Enter your choice no:"))
     
     if choice == 1:
-        print("'Add Task' selected")
-
-        tital = input("Enter task tital:")
-
-        task ={
-            "tital": tital,
-            "completed": False
-        }
-        tasks.append(task)
+        add_task()
 
     elif choice == 2:
-        print("'View Tasks' selected")
-
-        if tasks == []:
-                    print("No tasks found.")
-
-        for i in tasks:
-            if i["completed"] == True:
-                print(i["tital"] + "-- completed")
-            else:
-                print(i["tital"] + "-- incomplete")
+        view_task()
                 
     elif choice == 3:
-        print("'Mark Task as Completed' selected")
-        found = False
-
-        for i in tasks:
-            print(i["tital"])
-
-        select_completed_task = input("Select completed task:")
-
-        for i in tasks:
-            if i["tital"] == select_completed_task:
-                found = True
-                i["completed"] = True
-
-        if not found:
-            print("Task not found.")
+        mark_task_as_completed()
 
     elif choice == 4:
-        print("'Delete Task' selected")
-        found = False
-
-        for i in tasks:
-            print(i["tital"])
-
-        select_task_to_delete = input("Select task to delete:")
-
-        for i in tasks:
-            if i["tital"] == select_task_to_delete:
-                found = True
-                tasks.remove(i)
-
-        if not found:
-            print("Task not found.")
+        delete_task()
     
     elif choice == 5:
-        print("DONE, Complete your tasks as soon as possible")
+        print("DONE, Complete your tasks as soon as possible...")
 
     else:
         print("Invalid choice")
